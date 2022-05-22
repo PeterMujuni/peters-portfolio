5c096af9778c0b57dd1ed406e19b13c6ba0889b9
PeterMujuniMwebesa <kontakt@petermujuni.dk>
2022-05-22 17:14:04 +0200

    cleaned up

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,12 @@
+import Button from "../Button/Button";
 import Title from "../Title/Title";
 
 export default function Main() {
   return (
     <main className="main">
         <Title />
+        <Button />
+        <Button />
     </main>
   )
 }
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './Title.css'
 
 export default function Title() {
     const title = 'Peter Mujuni Mwebesa'
   return (
-    <h1>{title}</h1>
+    <>
+      <h1 className='title'>{title}</h1>
+      <h2 className='title--subtitle'>Full stack Developer</h2>
+      <h3 className='title--website'>www.petermujuni.dk</h3>
+    </>
   )
 }
